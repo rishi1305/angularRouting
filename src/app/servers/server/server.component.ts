f9ec97c641716aa6ed440e88c756dd0dad82e9b7
Kafka_Mongo <Kafka_Mongo@KafkaMongoDB-K.India.XoriantCorp.com>
2018-10-23 00:07:29 -0700

    First Commit for routing

---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService, private routes: ActivatedRoute) { }
+
+  ngOnInit() {
+    // const serverId = this.routes.snapshot.params["id"];
+    // this.server = this.serversService.getServer(serverId);
+    this.routes.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
+
+  }
+
+}
